cfcd3465002e5f4f4e7c681400e57b86c2492902
Bhadresh Modi <BModi@casebank.com>
2019-11-10 01:31:11 -0500

    Implemented organism class with random reproduce function

---
+++ public/test.genetics-model.js
@@ -0,0 +1,8 @@
+import Allele from './genetics-model.js';
+
+describe('Allele', function () {
+  it('has a getter name', function () {
+    let a = new Allele("blue");
+    assert.equal('xyz', 'bar', 'foo equal `bar`');
+  });
+});

528c97d366abf67ffc4817111607c105832c3470
Bhadresh Modi <BModi@casebank.com>
2019-11-18 09:47:35 -0500

    Read values from fields, create organism and log output of reproduce

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,3 +1,19 @@
+import Organism from '../scripts/organism.js';
+
 export default function createOffspring() {
-    console.log("hello");
+    let genes_a1 = document.getElementById("genes_a1").value.split(',');
+    let genes_a2 = document.getElementById("genes_a2").value.split(',');
+    let genes_b1 = document.getElementById("genes_b1").value.split(',');
+    let genes_b2 = document.getElementById("genes_b2").value.split(',');
+
+    let maleOrganism = new Organism( [
+        {chromosomeFromMaleParent: genes_a1, chromosomeFromFemaleParent: genes_a2}
+      ] );
+      
+    let femaleOrganism = new Organism( [
+        {chromosomeFromMaleParent: genes_b1, chromosomeFromFemaleParent: genes_b2}
+    ] );    
+
+    let offspring = maleOrganism.reproduce(femaleOrganism);
+    console.log("["+offspring.chromosomes[0].chromosomeFromMaleParent + "], [" + offspring.chromosomes[0].chromosomeFromFemaleParent+"]");
 }
